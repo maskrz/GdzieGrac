4726692d9e56eb22e06edf922a8fb0257b284c90
Maciej Skrzypinski <maskrz@gmail.com>
2014-05-19 21:56:59 +0200

    initial release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GdzieGrac::Application.config.secret_key_base = '8318d97d9224d0796a2c6a2eedac4f85777f2c5692b51939017956e12e70723299ed0f72e9006a32b9d5c9bed417c7acd8c8ab1925fb92a2cb79bf898d67a55c'
